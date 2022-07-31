dc65c7deef9a43cc1c1c5cb311473c8129dc3a42
JenosPaladin2 <sumach.kumar@gmail.com>
2022-07-31 10:56:13 +0200

    Hello World

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+class Hello
+{
+
+    /**
+     * Wieso braucht man static void Main?
+     * Wieso braucht man string[] args???
+     */
+    static void Main(string[] args)
+    {
+
+        //Print something in Terminal console
+        //Console.WriteLine("Hello World");
+        //Console.ReadKey();
+
+        Console.WriteLine("Types Sample");
+
+        //Declare int variable
+        int i = 34;
+
+        //char variable
+        char c = 'm';
+
+        //float variable
+        float per = 6.8f;
+
+        //test
+        //object o = c;
+        object o = c;
+
+        //String variable
+        string name = "JenosPaladin";
+
+        int counter = i + 1;
+
+        /*
+         * Use c in a condition. Check if the value of c is 'm'
+         * then print c and counter
+        */
+
+        if (c == 'm')
+        {
+            Console.WriteLine("i:" + i);
+            Console.WriteLine("counter:" + counter);
+            Console.WriteLine("c:" + c);
+            Console.WriteLine("name:" + name);
+            Console.WriteLine(counter);
+            Console.WriteLine("per:" + per);
+
+
+            //How to check which type is a variable???
+        }
+
+        Console.ReadLine();
+    }
+}
