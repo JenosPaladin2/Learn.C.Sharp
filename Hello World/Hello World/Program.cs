c571efe44f050a92144f5b130397db9d59fc6120
JenosPaladin2 <sumach.kumar@gmail.com>
2022-08-01 21:04:38 +0200

    Kommentare ergänzt

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -59,6 +59,8 @@ class Hello
          * This conversion occurs automatically.
          * 
          * DE: Implizite Konvertierung bedeutet, dass die Daten mit geringerer Genauigkeit ohne Datenverlust in Daten mit höherer Genauigkeit umgewandelt werden. Diese Umwandlung erfolgt automatisch.
+         * Einfache Sprache:
+         * Implizite Konvertierung bedeutet, dass ein Wert einer Variable zu einem gleichen oder höheren Variablentyp umgewandelt wird.
          */
         
         int x = 4568;
@@ -69,12 +71,70 @@ class Hello
          * What is explicit conversion?
          * Explicit conversion requires an explicit cast. This is done by putting the type in parentheses (). This conversion is not always successful. If successful, it may or may not be converted, with possible loss of data.
          * 
-         * DE: Eine explizite Konvertierung erfordert einen expliziten Cast. Dies geschieht, indem der Typ in Klammern () gesetzt wird. Diese Konvertierung ist nicht immer erfolgreich. Wenn Sie erfolgreich ist, kann es sein, dass sie nicht konvertiert wird, was zu Datenverlusten führen kann.
+         * DE: Eine explizite Konvertierung erfordert einen expliziten Cast. Dies geschieht, indem der Typ in Klammern () gesetzt wird.
+         * Diese runden Klammern werden auch als "Typkonvertierungsoperator" bezeichnet.
+         * 
+         * Folgende Syntax wird verwendet: (Zieldatentyp)Ausdruck
+         * 
+         * Diese Konvertierung ist nicht immer erfolgreich. Wenn Sie erfolgreich ist, kann es sein, dass sie nicht konvertiert wird, was zu Datenverlusten führen kann.
+         * 
+         * 
+         * Einfache Sprache: Datenumwandlung in einen anderen Typ, ohne darauf zu achten, ob der neue Typ gleich oder höherwertig ist. Dadurch kann man zu (theoretisch) allen kompatiblen Typen wechseln.
+         * Achtung: Beim Konvertieren von einer gleich oder höherwertigen zu einer niedrigen Typ kann ein Datenverlust entstehen!
+         */
+
+        int geradeZahl = 45689; //gleicher Typ
+        short expliziteZahl = (short)geradeZahl; // niedriger Typ
+        
+        double d = 3.14314256; // gleicher Typ
+        float f = (float)d; //niedriger Typ
+
+        /**
+         * Was ist der Unterschied zwischen implizite Konvertierung und expliziter Konvertierung?
+         * 
+         * Implizite Konvertierung: Typkonvertierung --> gleich oder höherwertigem Variablentyp
+         * Explizit Konvertierung: Typkonvertierung --> alle möglich, sofern kompatibel. Es kann Datenverlust entstehen (von gleich/hoch zu niedrig). 
+         */
+
+
+        /**
+         * Wie kann man Datentypen konvertieren, die miteinander nicht kompatibel sind?
+         * 
+         * Man nutzt die Klasse "Convert". Einige Funktionen dieser Klasse sind (siehe Openbook C#):
+         * 
+         * Methode                      Beschreibung
+         * 
+         * ToBoolean(Ausdruck)          Konvertiert den Ausdruck in einen bool-Typ.
+         * ToByte(Ausdruck)             Konvertiert den Ausdruck in einen byte-Typ.
+         * ToChar(Ausdruck)             Konvertiert den Ausdruck in einen char-Typ.
+         * ToDecimal(Ausdruck)          Konvertiert den Ausdruck in einen decimal-Typ.
+         * ToDouble(Ausdruck)           Konvertiert den Ausdruck in einen double-Typ.
+         * ToInt16(Ausdruck)            Konvertiert den Ausdruck in einen short-Typ.
+         * ToInt32(Ausdruck)            Konvertiert den Ausdruck in einen int-Typ.
+         * ToInt64(Ausdruck)            Konvertiert den Ausdruck in einen long-Typ.
+         * ToSByte(Ausdruck)            Konvertiert den Ausdruck in einen sbyte-Typ.
+         * ToSingle(Ausdruck)           Konvertiert den Ausdruck in einen float-Typ.
+         * ToString(Ausdruck)           Konvertiert den Ausdruck in einen string-Typ.
+         * ToUInt16(Ausdruck)           Konvertiert den Ausdruck in einen ushort-Typ.
+         * ToUInt32(Ausdruck)           Konvertiert den Ausdruck in einen uint-Typ.
+         * ToUInt64(Ausdruck)           Konvertiert den Ausdruck in einen ulong-Typ.
+         * 
+         * Wobei Ausdruck den Wert einer Variable darstellt.
+         * 
+         * Bsp:
+         * 
+         * long val1 = 5642;
+         * int val2 = Convert.ToInt32(val1); //Convert Klasse mit Methode ToInt32 genutzt.
+         * 
+         * Ist gleichwertig mit
+         * 
+         * long val1 = 5642;
+         * int val2 = (int) val1 //explizite Konvertierung
+         * 
          * 
          */
 
 
 
-        
     }
 }
